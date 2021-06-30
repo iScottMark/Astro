59cb6f595e244bbbb56c3fa1c3d8bd1cb51bbc92
ScottMark <xiaoma_hebut@163.com>
2021-06-30 10:41:52 +0800

    deploy

---
+++ assets/js/13.cc284190.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{378:function(t,n,e){"use strict";e.r(n);var s=e(25),o=Object(s.a)({},(function(){var t=this.$createElement,n=this._self._c||t;return n("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[n("p",[this._v("对一些常见的光谱数据处理方法进行介绍，并利用 Python 给出具体代码，帮助大家完成最佳实践。")])])}),[],!1,null,null,null);n.default=o.exports}}]);
